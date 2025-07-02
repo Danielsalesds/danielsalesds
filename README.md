f9ffb290f2c454fa38dc7877ef0576aa1ce48377
Francisco Daniel <francisco.daniel.108@ufrn.edu.br>
2025-07-02 18:32:23 -0300

    implementado uma api para envio de mansagem de contado

---
+++ README.md
@@ -0,0 +1,2 @@
+Este portfólio está em constante atualização! 
+Volte em breve para ver novidades.
--- scripts.js
+++ scripts.js
@@ -37,3 +37,36 @@ const projects = {
   function closeModal() {
     document.getElementById('modal').style.display = 'none';
   }
+
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "✅ Obrigado! Sua mensagem foi enviada com sucesso!";
+      form.reset();
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+        } else {
+          status.innerHTML = "Oops! Ocorreu um problema ao enviar sua mensagem.";
+        }
+      });
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! Ocorreu um problema ao enviar sua mensagem.";
+  });
+}
+
+form.addEventListener("submit", handleSubmit);
